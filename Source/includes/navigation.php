5de30adb7dd38f939a05acd293eecf939c3f0dd6
Joshua Thompson <ysovuka@gmail.com>
2015-11-08 12:10:43 -0600

    Navigation Changes
    Changed the navigation button to be dynamic.

--- Source/includes/navigation.php
+++ Source/includes/navigation.php
@@ -1,8 +1,10 @@
+<?php require_once('scripts/strings.php'); ?>
+
 <img src="resources/tpslogo.png" class="logo" alt="TPS">
 <a href="#" class="myButton">Search</a>
 <a href="#" class="myButton">Contract</a>
 <a href="#" class="myButton">Request</a>
 <a href="#" class="myButton">Members</a>
-<a href="register.php" class="myButton">Register</a>
-<a href="login.php" class="myButton">Login</a>
-<a href="index.php" class="currentButton">Home</a>
+<a href="register.php" class="<?php if (endsWith($_SERVER["PHP_SELF"], "register.php")) { echo currentButton ;} else { echo myButton; } ?>">Register</a>
+<a href="login.php" class="<?php if (endsWith($_SERVER["PHP_SELF"], "login.php")) { echo currentButton; } else { echo myButton; } ?>">Login</a>
+<a href="index.php" class="<?php if (endsWith($_SERVER["PHP_SELF"], "index.php")) { echo currentButton; } else { echo myButton; } ?>">Home</a>
