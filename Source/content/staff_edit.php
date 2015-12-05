26d773eb3fa0b867e082ed82ad58a4201bad4b46
Gimic69 <chzcha1234@yahoo.com>
2015-12-05 11:50:01 -0600

    Finished UI
    Finished UI, may be a couple small issues that will be worked out in
    testing.

--- Source/content/staff_edit.php
+++ Source/content/staff_edit.php
@@ -13,7 +13,7 @@
 if (isset($message)) { ?> <div id="message" align="center" style="margin:0px; padding-top:2.5px;"><?php echo $message ?></div><!-- End of Message --><?php ; }?>
 
 <div id="view">
-	<div>
+	<div style="margin-bottom: 10px;">
 		<form action="?view=<?php echo $_SESSION['id']; ?>" method="POST">
 			<input type="submit" id="submit" name="submit" value="View Profile" />
 		</form>
@@ -21,30 +21,33 @@ if (isset($message)) { ?> <div id="message" align="center" style="margin:0px; pa
     
 	<div style="float: left;">
 		<?php if (isset($staff->picture) && !empty($staff->picture) && file_exists("uploads/pictures/" . $staff->picture)){ ?>
-        	<img id="pic" name="pic" src="uploads/pictures/<?php echo $staff->picture; ?>" />
+        	<img class="image" id="pic" name="pic" src="uploads/pictures/<?php echo $staff->picture; ?>" />
 		<?php } ?>
-		<form method="POST" action="<?php $_SERVER["PHP_SELF"]; ?>?edit" enctype="multipart/form-data">
+		<?php if (isset($staff->picture) && empty($staff->picture)){ ?>
+        	<img class="image" id="pic" name="pic" src="uploads/pictures/noperson.png<?php echo $staff->picture; ?>" />
+		<?php } ?>
+		<form style="margin-top:5px;" method="POST" action="<?php $_SERVER["PHP_SELF"]; ?>?edit" enctype="multipart/form-data">
 		    <input type="hidden" name="type" value="picture" />
 		    <input type="file" name="picture" id="picture" required/><br />
 			<input type="submit" value="Upload" name="submit" id="submit" />
 		</form><br />
 	</div><!-- End of Upload Picture -->
 
-	<div id="infoBox">
+	<div id="editInfoBox">
     	<div style="float:left;">
 			<form id="account" name="account" action="<?php $_SERVER['PHP_SELF']; ?>?edit" method="POST">
 			    <input type="hidden" name="type" value="account" />
 			    <div id="eMail" name="eMail">
 			    	<label>E-Mail:</label><br />
-			        <input type="email" id="email" name="email" placeholder="E-Mail Address" value="<?php echo $user->email; ?>" /><br />
+			        <input style="margin-bottom:5px;" type="email" id="email" name="email" placeholder="E-Mail Address" value="<?php echo $user->email; ?>" /><br />
 			    </div><!-- End of Email -->
     			<div id="password" name="password">
 					<label>Current Password:</label><br />
-			        <input type="password" title="Field cannot be left blank." pattern="\w+" placeholder="Password" id="current_pwd" name="current_pwd" required><br />
+			        <input style="margin-bottom:5px;" type="password" title="Field cannot be left blank." pattern="\w+" placeholder="Password" id="current_pwd" name="current_pwd" required><br />
 			    	<label>Password:</label><br />
-			        <input placeholder="Password" id="pwd" name="pwd" title="Password must contain at least 6 characters, including UPPER/lowercase and numbers." type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"><br />
+			        <input style="margin-bottom:5px;" placeholder="Password" id="pwd" name="pwd" title="Password must contain at least 6 characters, including UPPER/lowercase and numbers." type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"><br />
 			    	<label>Confirm Password:</label><br />
-			        <input title="Please enter the same Password as above." type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" placeholder="Confirm Password" id="confirm_pwd" name="confirm_pwd"><br />
+			        <input style="margin-bottom:10px;" title="Please enter the same Password as above." type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" placeholder="Confirm Password" id="confirm_pwd" name="confirm_pwd"><br />
 			    </div><!-- End of Password -->
 			    <input type="submit" id="submit" value="Submit" name="submit" />
 			</form><br />
@@ -62,33 +65,33 @@ if (isset($message)) { ?> <div id="message" align="center" style="margin:0px; pa
 
 			<div id="information" name="information">
 		    	<label>First Name:</label><br />
-        		<input type="text" id="Fname" name="Fname" value="<?php echo $staff->Fname; ?>" required/><br />
+        		<input style="margin-bottom:5px;" type="text" id="Fname" name="Fname" value="<?php echo $staff->Fname; ?>" required/><br />
 		        <label>Last Name:</label><br />
-		        <input type="text" id="Lname" name="Lname" value="<?php echo $staff->Lname; ?>" required/><br />
+		        <input style="margin-bottom:5px;" type="text" id="Lname" name="Lname" value="<?php echo $staff->Lname; ?>" required/><br />
 		        <label>City:</label><br />
-		        <input id="city" name="city" value="<?php echo $staff->city; ?>"  required/><br />
+		        <input style="margin-bottom:5px;" id="city" name="city" value="<?php echo $staff->city; ?>"  required/><br />
 		        <label>State:</label><br />
-		        <select id="state" name="state" required><option value="0">Choose a state</option><?php echo listStates($staff->state); ?></select><br />
+		        <select style="margin-bottom:5px;" id="state" name="state" required><option value="0">Choose a state</option><?php echo listStates($staff->state); ?></select><br />
         		<label>Zip Code:</label><br />
-		        <input type="text" pattern="[0-9]{5}" id="zip" name="zip" value="<?php echo $staff->zip; ?>" required/><br />
+		        <input style="margin-bottom:5px;" type="text" pattern="[0-9]{5}" id="zip" name="zip" value="<?php echo $staff->zip; ?>" required/><br />
             </div><!-- End of Information -->
             
             <div>
 		    	<label>Type of Work:</label><br />
-		        <select id="workType" name="workType" required>
+		        <select style="margin-bottom:5px;" id="workType" name="workType" required>
 		        	<option value="Professional" <?php echo (($staff->workType === "Professional") ? "selected" : ""); ?>>Professional</option>
         		    <option value="Scientific" <?php echo (($staff->workType === "Scientific") ? "selected" : ""); ?>>Scientific</option>
 		        </select><br />
 		    	<label>Experience:</label><br />
-		        <input type="number" max="99" min="1.00" id="experience" name="experience" value="<?php echo $staff->experience; ?>" required /><br />
+		        <input style="margin-bottom:5px;" type="number" max="99" min="1.00" id="experience" name="experience" value="<?php echo $staff->experience; ?>" required /><br />
         		<label>Education:</label><br />
-				<select id="education" name="education" required>
+				<select style="margin-bottom:5px;" id="education" name="education" required>
 		            <option value="0" <?php echo (($staff->education === "0") ? "selected" : ""); ?>>No Degree</option>
 		            <option value="1" <?php echo (($staff->education === "1") ? "selected" : ""); ?>>High School</option>
 					<option value="2" <?php echo (($staff->education === "2") ? "selected" : ""); ?>>College</option>
 		        </select><br />
 		        <label>Salary:</label><br />
-		        <input type="text" pattern="^[1-9]\d*\.[0-9]{2}" id="salary" name="salary" value="<?php echo $staff->salary; ?>" required/>
+		        <input style="margin-bottom:10px;" type="text" pattern="^[1-9]\d*\.[0-9]{2}" id="salary" name="salary" value="<?php echo $staff->salary; ?>" required/>
 			</div><!-- End of Work Information -->
     
 		    <div id="submit_form">
@@ -100,7 +103,7 @@ if (isset($message)) { ?> <div id="message" align="center" style="margin:0px; pa
 	<form method="POST" action="<?php $_SERVER["PHP_SELF"]; ?>?edit" enctype="multipart/form-data">
 		<table>
 			<tr>
-        		<th><label>Resume:</label></th>
+        		<td><label><b>Resume:</b></label></td>
 	        </tr>
             <tr>
             	<td>
